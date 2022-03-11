5edfc277334d47504da19a0e7e17166d6c80142e
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-03-11 15:03:54 -0500

    Changed parameters.

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ inference = True
 epochs = 1
 
 # Optimization parameters:
-num_training_iterations = 10000  # Increase this to train longer
-batch_size = 4  # Experiment between 1 and 64
+num_training_iterations = 2000  # Increase this to train longer
+batch_size = 8  # Experiment between 1 and 64
 seq_length = 100  # Experiment between 50 and 500
-learning_rate = 5e-2  # Experiment between 1e-5 and 1e-1
+learning_rate = 5e-3  # Experiment between 1e-5 and 1e-1
 
 # Model parameters:
 embedding_dim = 256
