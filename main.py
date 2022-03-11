59578e373dcb17d5cfcd7dd0a1f5757d4206281b
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-03-11 15:50:27 -0500

    Changed parameters. About to merge to master.

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ epochs = 1
 
 # Optimization parameters:
 num_training_iterations = 2000  # Increase this to train longer
-batch_size = 8  # Experiment between 1 and 64
+batch_size = 64  # Experiment between 1 and 64
 seq_length = 100  # Experiment between 50 and 500
-learning_rate = 5e-3  # Experiment between 1e-5 and 1e-1
+learning_rate = 1e-1  # Experiment between 1e-5 and 1e-1
 
 # Model parameters:
 embedding_dim = 256
