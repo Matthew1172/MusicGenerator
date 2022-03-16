1226909ba089ae8daaeb2ef336b7c480567c0793
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-03-16 02:37:36 -0400

    Moved everything to device 0 for testing

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -7,19 +7,19 @@ class MyLSTM(torch.nn.Module):
         self.dev0 = dev0
         self.dev1 = dev1
         self.embedding = torch.nn.Embedding(batch_size*seq_length, embedding_dim).to(dev0)
-        self.lstm = torch.nn.LSTM(batch_first=True, input_size=embedding_dim, hidden_size=rnn_units).to(dev1)
+        self.lstm = torch.nn.LSTM(batch_first=True, input_size=embedding_dim, hidden_size=rnn_units).to(dev0)
         self.linear = torch.nn.Linear(in_features=rnn_units, out_features=vocab_size).to(dev0)
 
     def forward(self, x, hn, cn):
         x = x.to(self.dev0)
-        hn = hn.to(self.dev1)
-        cn = cn.to(self.dev1)
+        hn = hn.to(self.dev0)
+        cn = cn.to(self.dev0)
 
         embeds = self.embedding(x)
         embeds = embeds.to(self.dev0)
         # Stateful
         embeds_longer = embeds.view(1,embeds.shape[0]*embeds.shape[1], embeds.shape[2])
-        embeds_longer = embeds_longer.to(self.dev1)
+        embeds_longer = embeds_longer.to(self.dev0)
         out_longer, (hn, cn) = self.lstm(embeds_longer, (hn.detach(), cn.detach()))
         hn = hn.to(self.dev0)
         cn = cn.to(self.dev0)
