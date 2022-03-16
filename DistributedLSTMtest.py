d4f588d74a3ca95f9e80713eca13ed7cf8dc22d9
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-03-16 01:46:56 -0400

    fixing bug

--- DistributedLSTMtest.py
+++ DistributedLSTMtest.py
@@ -182,13 +182,13 @@ def demo_checkpoint(rank, world_size):
 
     CHECKPOINT_PATH = checkpoint_prefix
 
-    '''
     if rank == 0:
         # All processes should see same parameters as they all start from same
         # random parameters and gradients are synchronized in backward passes.
         # Therefore, saving it in one process is sufficient.
         torch.save(ddp_model.state_dict(), CHECKPOINT_PATH)
 
+    '''
     # Use a barrier() to make sure that process 1 loads the model after process
     # 0 saves it.
     dist.barrier()
