88ace7d11166a487cb0eb34b6be7484d122849d6
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-03-25 17:13:06 -0400

    Increased number of songs to generate and generation length.

--- generate.py
+++ generate.py
@@ -9,8 +9,9 @@ from music21 import *
 dataset = "./dataset/set1"
 seed = 1
 temp = 1.0
-gen_length = 50
+gen_length = 500
 log_interval = 200
+numberOfSongs = 10
 
 # Checkpoint location:
 cwd = os.getcwd()
@@ -59,70 +60,70 @@ input = torch.randint(ntokens, (1, 1), dtype=torch.long).to(device)
 
 '''TODO: create music 21 score'''
 
-generatedSong = []
-generatedSong.append(corpus.dictionary.idx2word[seed])
-with torch.no_grad():  # no tracking history
-    for i in tqdm(range(gen_length)):
-        output = model(input, False)
-        word_weights = output[-1].squeeze().div(temp).exp().cpu()
-        word_idx = torch.multinomial(word_weights, 1)[0]
-        word_tensor = torch.Tensor([[word_idx]]).long().to(device)
-        input = torch.cat([input, word_tensor], 0)
+for i in range(numberOfSongs):
+    generatedSong = []
+    generatedSong.append(corpus.dictionary.idx2word[seed])
+    with torch.no_grad():  # no tracking history
+        for i in tqdm(range(gen_length)):
+            output = model(input, False)
+            word_weights = output[-1].squeeze().div(temp).exp().cpu()
+            word_idx = torch.multinomial(word_weights, 1)[0]
+            word_tensor = torch.Tensor([[word_idx]]).long().to(device)
+            input = torch.cat([input, word_tensor], 0)
 
-        word = corpus.dictionary.idx2word[word_idx]
+            word = corpus.dictionary.idx2word[word_idx]
 
-        #outf.write(word + ('\n' if i % 20 == 19 else ''))
-        #outf.write(word)
-        generatedSong.append(word)
+            # outf.write(word + ('\n' if i % 20 == 19 else ''))
+            # outf.write(word)
+            generatedSong.append(word)
 
-        if i % log_interval == 0:
-            print('| Generated {}/{} words'.format(i, gen_length))
+            if i % log_interval == 0:
+                print('| Generated {}/{} words'.format(i, gen_length))
 
+    '''TODO: construct a time signature, clef, and key signature object and append it to the score'''
+    '''word is either a clef, key signature, time signature, note, or barline'''
+    print(generatedSong)
 
-'''TODO: construct a time signature, clef, and key signature object and append it to the score'''
-'''word is either a clef, key signature, time signature, note, or barline'''
-print(generatedSong)
-
-p = stream.Part()
-m = stream.Measure()
-for i in generatedSong:
-    if i == "|":
-        p.append(m)
-        m = stream.Measure()
-    else:
-        j = i.split(" ")
-        if "Note" in j:
-            name = j[1]
-            try:
+    p = stream.Part()
+    m = stream.Measure()
+    for i in generatedSong:
+        if i == "|":
+            p.append(m)
+            m = stream.Measure()
+        else:
+            j = i.split(" ")
+            if "Note" in j:
+                name = j[1]
+                try:
+                    length = float(j[2])
+                except(ValueError):
+                    length = Fraction(j[2])
+                m.append(note.Note(nameWithOctave=name, quarterLength=length))
+            elif "Rest" in j:
                 length = float(j[2])
-            except(ValueError):
-                length = Fraction(j[2])
-            m.append(note.Note(nameWithOctave=name, quarterLength=length))
-        elif "Rest" in j:
-            length = float(j[2])
-            m.append(note.Rest(quarterLength=length))
-        elif "Rep" in j:
-            direction = j[2]
-            m.append(bar.Repeat(direction=direction))
-        elif "Bar" in j:
-            type = j[1]
-            m.append(bar.Barline(type=type))
-        elif "Clef" in j:
-            if j[1] == 'G':
-                m.append(clef.TrebleClef())
+                m.append(note.Rest(quarterLength=length))
+            elif "Rep" in j:
+                direction = j[2]
+                m.append(bar.Repeat(direction=direction))
+            elif "Bar" in j:
+                type = j[1]
+                m.append(bar.Barline(type=type))
+            elif "Clef" in j:
+                if j[1] == 'G':
+                    m.append(clef.TrebleClef())
+                else:
+                    continue
+            elif "Key" in j:
+                sharps = int(j[1])
+                m.append(key.KeySignature(sharps=sharps))
+            elif "Time" in j:
+                numerator = j[1]
+                denominator = j[2]
+                tsig = numerator + "/" + denominator
+                m.append(meter.TimeSignature(tsig))
             else:
                 continue
-        elif "Key" in j:
-            sharps = int(j[1])
-            m.append(key.KeySignature(sharps=sharps))
-        elif "Time" in j:
-            numerator = j[1]
-            denominator = j[2]
-            tsig = numerator+"/"+denominator
-            m.append(meter.TimeSignature(tsig))
-        else:
-            continue
-
-p.write("text", GENERATION_PREFIX+".txt")
-p.write("musicxml", GENERATION_PREFIX+".mxl")
-p.write("midi", GENERATION_PREFIX+".mid")
+    out = GENERATION_PREFIX + "_"+str(i)
+    p.write("text", out + ".txt")
+    p.write("musicxml", out + ".mxl")
+    p.write("midi", out + ".mid")
