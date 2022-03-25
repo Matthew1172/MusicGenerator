feb9d59da1e9dbca427fea742886b521422a5412
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-03-25 03:10:08 -0400

    added mean loss

--- main.py
+++ main.py
@@ -142,9 +142,9 @@ def train():
             cur_loss = total_loss / log_interval
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-                    'loss {:5.2f} | ppl {:8.2f}'.format(
+                    'loss {:5.2f} | ppl {:8.2f} | meanloss: '.format(
                 epoch, batch, len(train_data) // bptt, lr,
-                elapsed * 1000 / log_interval, cur_loss, math.exp(cur_loss)))
+                elapsed * 1000 / log_interval, cur_loss, math.exp(cur_loss), loss.cpu().detach().numpy().mean()))
             total_loss = 0
             start_time = time.time()
 
