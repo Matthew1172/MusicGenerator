ccc25569a4aed0ab20c78c2d008419c967794558
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-09 16:01:07 -0400

    added test generate route

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import os.path
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify, make_response
 from Generation import *
+from music21 import *
 
 app = Flask(__name__)
+
+# INITIALIZATION
+xml_response_headers = {"Content-Type": "text/xml",
+                        "charset":      "utf-8"
+                        }
+
+#use this parameter or extract it from the metadata somehow
+timesignature = meter.TimeSignature('4/4')
 
 '''
 request looks like:
@@ -69,3 +78,56 @@ def midi():
         file = request.args.get('file')
         path = os.path.join(os.getcwd(), os.path.join("outputs", os.path.join(folder, file)))
         return send_file(path, mimetype='audio/midi')
+
+
+
+@app.route('/test-generate', methods=['GET'])
+def ex():
+    folder = request.args.get('folder')
+    file = request.args.get('file')
+    path = os.path.join(os.getcwd(), os.path.join("outputs", os.path.join(folder, file)))
+    _current_sheet = converter.parse(path)
+    return sheet_to_xml_response(_current_sheet)
+
+
+def insert_musicxml_metadata(sheet: stream.Stream):
+    """
+    Insert various metadata into the provided XML document
+    The timesignature in particular is required for proper MIDI conversion
+    """
+    global timesignature
+
+    from music21.clef import TrebleClef, BassClef, Treble8vbClef
+    for part, name, clef in zip(
+            sheet.parts,
+            ['soprano', 'alto', 'tenor', 'bass'],
+            [TrebleClef(), TrebleClef(), Treble8vbClef(), BassClef()]
+    ):
+        # empty_part = part.template()
+        part.insert(0, timesignature)
+        part.insert(0, clef)
+        part.id = name
+        part.partName = name
+
+    md = metadata.Metadata()
+    sheet.insert(0, md)
+
+    # required for proper musicXML formatting
+    sheet.metadata.title = 'mgen'
+    sheet.metadata.composer = 'mgen'
+
+def sheet_to_xml_bytes(sheet: stream.Stream):
+    """Convert a music21 sheet to a MusicXML document"""
+    # first insert necessary MusicXML metadata
+    insert_musicxml_metadata(sheet)
+
+    sheet_to_xml_bytes = musicxml.m21ToXml.GeneralObjectExporter(sheet).parse()
+
+    return sheet_to_xml_bytes
+
+def sheet_to_xml_response(sheet: stream.Stream):
+    """Generate and send XML sheet"""
+    xml_sheet_bytes = sheet_to_xml_bytes(sheet)
+
+    response = make_response((xml_sheet_bytes, xml_response_headers))
+    return response
