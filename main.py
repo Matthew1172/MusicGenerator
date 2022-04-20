40184dc8a883e7aaaea0fcf35be758dea44655d2
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-20 02:15:22 -0400

    added a scheduler

--- main.py
+++ main.py
@@ -132,9 +132,9 @@ def train():
             pass
         cur_loss = total_loss / log_interval
         elapsed = time.time() - start_time
-        print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+        print('| epoch {:3d} | {:5d}/{:5d} batches | lr {} | ms/batch {:5.2f} | '
                 'loss {:5.2f} | ppl {:8.2f} | meanloss: {:5.2f}'.format(
-            epoch, batch, len(train_data) // bptt, scheduler.get_last_lr(),
+            epoch, batch, len(train_data) // bptt, scheduler.get_lr(),
             elapsed * 1000 / log_interval, cur_loss, math.exp(cur_loss), loss.cpu().detach().numpy().mean()))
         total_loss = 0
         start_time = time.time()
