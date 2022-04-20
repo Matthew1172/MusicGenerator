5efa128cd855e4d58d6827a5a2c9e24c5f980204
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-20 01:30:37 -0400

    pushing to server

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 print("Device is now: ", device)
 
 #size of word embeddings
-emsize = 512
+emsize = 256
 #number of hidden units per layer
 hidden_units = 1024
 #number of layers
@@ -35,7 +35,7 @@ dropout = 2e-1
 #report interval
 log_interval = 200
 #the number of heads in the encoder/decoder of the transformer model
-num_heads = 4
+num_heads = 2
 #model = TransformerModel(ntokens, emsize, num_heads, hidden_units, nlayers, device, device, dropout).to(device)
 loss_fn = "NLL"
 opt = "ADAM"
