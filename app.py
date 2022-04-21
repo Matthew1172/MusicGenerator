63497440c438c9708cff5332957e28386d5e5e3c
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-21 19:27:01 -0400

    removed /predict route added print lines for debugging

--- app.py
+++ app.py
@@ -74,81 +74,6 @@ def notes():
 request looks like:
 req = {
     dataset: "Good"
-    input_clef: "Clef G"
-    input_key: "Key 2"
-    input_seq: "Note C 1.0"
-    input_time: "Time 4 4"
-    length: 100
-    random_clef: False
-    random_key: False
-    random_seq: False
-    random_seq_length: 1
-    random_time: False
-    songs: 1
-    temperature: 0.85
-    abc: "M:?\nV:1 name=?\nK:?\n?"
-}
-'''
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        content = request.json
-
-        DATASETS = "datasets"
-        content['dataset'] = os.path.join(DATASETS, content['dataset'])
-        g = Generation(**content)
-
-        try:
-            g.checkDataset()
-        except DatasetNotFound as dnf:
-            return jsonify({'error': str(dnf)})
-
-        g.loadModel()
-        g.loadDictionary()
-        g.setInitClef()
-        g.setInitKey()
-        g.setInitTime()
-        g.setInitSeq()
-
-        '''TODO: check for custom exceptions and return proper error codes.
-        Append all errors to one error code so the user can see everything at once.'''
-        try:
-            g.checkInitClef()
-            g.checkInitKey()
-            g.checkInitTime()
-            g.checkInitSeq()
-        except NoteNotFoundInDictionary as nnf:
-            return jsonify({'error': str(nnf)})
-        except ClefNotFoundInDictionary as cnf:
-            return jsonify({'error': str(cnf)})
-        except TimeNotFoundInDictionary as tnf:
-            return jsonify({'error': str(tnf)})
-        except KeyNotFoundInDictionary as knf:
-            return jsonify({'error': str(knf)})
-        except:
-            return jsonify({'error': "could not run generation with inputs."})
-
-        g.generate()
-        g.save()
-        midi = g.GENERATION_PREFIX+"_1.mid"
-        mxl = g.GENERATION_PREFIX+"_1.mxl"
-        if os.path.exists(mxl) and os.path.exists(midi):
-            mxl_path = mxl.split('\\')[-2:]
-            midi_path = midi.split('\\')[-2:]
-            if len(mxl_path) < 2:
-                mxl_path = mxl.split('/')[-2:]
-                midi_path = midi.split('/')[-2:]
-            return jsonify({
-                'mxl': mxl_path,
-                'midi': midi_path
-            })
-        else:
-            return jsonify({'saved': False})
-
-'''
-request looks like:
-req = {
-    dataset: "Good"
     length: 100
     random_seq_length: 1
     songs: 1
@@ -180,6 +105,11 @@ def mgen():
         g.setInitSeq()
 
         abc = content['abc']
+
+
+        print(abc)
+
+
         g.loadDataFromAbc(abc)
 
         '''TODO: check for custom exceptions and return proper error codes.
@@ -199,6 +129,11 @@ def mgen():
             return jsonify({'error': str(knf)})
         except:
             return jsonify({'error': "could not run generation with inputs."})
+
+
+        print(g.iSeq)
+
+
 
         g.generate()
         g.save()
