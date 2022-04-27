271ad6fa061b8c07505387a549f716eda27042ec
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-04-27 03:43:17 -0400

    pushing to server

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ device2 = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 print("Device is now: ", device)
 
 #size of word embeddings
-emsize = 1024
+emsize = 512
 #number of hidden units per layer
 hidden_units = 2048
 #number of layers
-nlayers = 2
+nlayers = 4
 #initial learning rate
 learning_rate = 1e-1
 #momentum for SGD
