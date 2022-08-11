d7fd9b22984d45b256a4d2ee5aa892c65a91d28c
Matthew Pecko <32465317+Matthew1172@users.noreply.github.com>
2022-08-10 20:24:18 -0400

    Master Addr env variable set

--- main.py
+++ main.py
@@ -87,6 +87,8 @@ class ToyModel(nn.Module):
 ###############################################################################
 
 def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
     # initialize the process group
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
